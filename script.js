9d1eac0a062f4f9419ba96d292ddfb96adf39431
gabriel barbosa <gabrielb_lima@hotmail.com>
2023-04-14 20:33:21 -0300

    testesteste

--- script.js
+++ script.js
@@ -162,18 +162,26 @@ function sendMessage(userName, messageText) {
   messagesContainer.appendChild(messageElement);
 
   // Send a POST request to the API endpoint to send a public message
-  axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', message)
-  .then(response => {
-    // If the server responds with a successful status (200), continue fetching and displaying messages
-    console.log(`Public message sent by user ${userName}.`);
-    // Fetch and display messages again to update the chat
-    fetchMessagesAndDisplay();
-  })
-  .catch(error => {
-    // Handle error for sending public message
-    console.error(`Error sending public message: ${error.message}`);
-    // Reload the page to go back to the name input step only if the error is not related to network issues
-    if (error.response) {
-      window.location.reload();
-    }
-  })};
+  // Send a POST request to the API endpoint to send a public message
+axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', message)
+.then(response => {
+  // If the server responds with a successful status (200), continue fetching and displaying messages
+  console.log(`Public message sent by user ${userName}.`);
+  // Append the sent message to the messages container immediately
+  const messageElement = document.createElement('li');
+  // Use innerHTML instead of textContent to interpret HTML tags
+  messageElement.innerHTML = `<strong>${message.from}</strong> ${message.text} ${message.time}`;
+  messagesContainer.appendChild(messageElement);
+  // Fetch and display messages again to update the chat
+  fetchMessagesAndDisplay();
+})
+.catch(error => {
+  // Handle error for sending public message
+  console.error(`Error sending public message: ${error.message}`);
+  // Reload the page to go back to the name input step only if the error is not related to network issues
+  if (error.response) {
+    window.location.reload();
+  }
+});
+}
+
